2a29a024080d9cd3a699c9909839cfba157b6b76
fadak <f9999cat.6.29kk@gmail.com>
2024-06-29 22:41:47 +0300

    vvvv

--- lib/language/locale_controller.dart
+++ lib/language/locale_controller.dart
@@ -7,10 +7,25 @@ class MyLocaleController extends GetxController {
 
   get initialLang => null;
 
+  @override
+  void onInit() {
+    super.onInit();
+    _loadLanguage();
+  }
+
   void changeLang(String codelang) async {
     Locale locale = Locale(codelang);
     Get.updateLocale(locale);
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setString(LANGUAGE_CODE, codelang);
+  }
+
+  void _loadLanguage() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? codelang = prefs.getString(LANGUAGE_CODE);
+    if (codelang != null) {
+      Locale locale = Locale(codelang);
+      Get.updateLocale(locale);
+    }
   }
 }
